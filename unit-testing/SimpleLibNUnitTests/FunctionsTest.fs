d974ecd413c586c325c27f61bfebb63b5b947e2f
Aleksandr Petrovskiy <aepetrovskiy@inno.tech>
2022-11-11 00:47:07 +0300

    the initial refactoring

--- unit-testing/SimpleLibNUnitTests/FunctionsTest.fs
+++ unit-testing/SimpleLibNUnitTests/FunctionsTest.fs
@@ -1,9 +1,10 @@
 [<NUnit.Framework.TestFixture>]
-module SimpleLibxUnitTests
+module SimpleLibNUnitTests
 
 
 open System
 open System.Collections.Generic
+// open Allure.Net.Commons
 open Allure.Commons
 open NUnit.Allure.Attributes
 open NUnit.Allure.Core
@@ -11,12 +12,11 @@ open NUnit.Framework
 
 open type NUnit.Framework.Assert
 
-open Allure.Xunit.Attributes
-open Xunit
+
 open SimpleLib
 
 
-// NUnit
+
 let factorialTestData () : IEnumerable<TestCaseData> =
     [ new TestCaseData(0, 1)
       new TestCaseData(1, 1)
@@ -24,34 +24,20 @@ let factorialTestData () : IEnumerable<TestCaseData> =
       new TestCaseData(3, 6)
       new TestCaseData(4, 24) ]
 
-// xUnit
-let factorialXunitTestData () : IEnumerable<(obj)[]> =
-    [ [| 0; 1 |]
-      [| 1; 1 |]
-      [| 2; 2 |]
-      [| 3; 6 |]
-      [| 4; 24 |] ]
 
 
 [<AllureEpic("Unit testing in F#")>]
 [<AllureFeature([| "Parameterized tests" |])>]
-[<AllureStory([| "xUnit" |])>]
-[<AllureSuite("xUnit")>]
-[<AllureTag([|  "xUnit"; |])>]
-[<AllureSeverity(SeverityLevel.normal)>]
+[<AllureStory([| "NUnit" |])>]
+[<AllureSuite("NUnit")>]
+[<AllureTag([| "NUnit"; |])>]
+// [<AllureSeverity(SeverityLevel.normal)>]
 [<AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")>]
-[<AllureId(123)>]
-[<AllureName("test0001 xUnit")>]
+// [<AllureId(123)>]
+[<AllureName("test0001 NUnit")>]
 
-// NUnit
 [<Test>]
 [<TestCaseSource("factorialTestData")>]
-
-// xUnit
-[<AllureXunitTheory>]
-[<MemberData(nameof (factorialXunitTestData))>]
-
-
 let factorialTest (number: int, expectedResult: int) =
     let actualResult = FuncLib.factorial number
     AreEqual(expectedResult, actualResult, $"Expected = {expectedResult}, actual = {actualResult}")
---
+++ unit-testing/SimpleLibNUnitTests/UnitTest1.fs
@@ -0,0 +1,11 @@
+module SampleTests
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
