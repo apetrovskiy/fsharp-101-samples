41eb1667652fba96cc8b12be567e93b1f073dbfc
Aleksandr Petrovskiy <aepetrovskiy@inno.tech>
2022-11-11 20:42:08 +0300

    experimental changes

--- unit-testing/SimpleLibNUnitTests/FunctionsTest.fs
+++ unit-testing/SimpleLibNUnitTests/FunctionsTest.fs
@@ -1,11 +1,13 @@
 [<NUnit.Framework.TestFixture>]
+[<NUnit.Allure.Core.AllureNUnit(false)>]
+[<NUnit.Allure.Attributes.AllureSuite("NUnit")>]
 module SimpleLibNUnitTests
 
 
 open System
 open System.Collections.Generic
-// open Allure.Net.Commons
-open Allure.Commons
+open Allure.Net.Commons
+// open Allure.Commons
 open NUnit.Allure.Attributes
 open NUnit.Allure.Core
 open NUnit.Framework
@@ -15,29 +17,48 @@ open type NUnit.Framework.Assert
 
 open SimpleLib
 
+// let factorialTestData () = //: IEnumerable<TestCaseData> =
+//     [ new TestCaseData(0, 1)
+//       new TestCaseData(1, 1)
+//       new TestCaseData(2, 2)
+//       new TestCaseData(3, 6)
+//       new TestCaseData(4, 24) ]
 
 
-let factorialTestData () : IEnumerable<TestCaseData> =
-    [ new TestCaseData(0, 1)
-      new TestCaseData(1, 1)
-      new TestCaseData(2, 2)
-      new TestCaseData(3, 6)
-      new TestCaseData(4, 24) ]
+type testData =
+
+    static member factorialTestData() = //: IEnumerable<TestCaseData> =
+        [ new TestCaseData(0, 1)
+          new TestCaseData(1, 1)
+          new TestCaseData(2, 2)
+          new TestCaseData(3, 6)
+          new TestCaseData(4, 24) ]
 
 
-
-[<AllureEpic("Unit testing in F#")>]
-[<AllureFeature([| "Parameterized tests" |])>]
-[<AllureStory([| "NUnit" |])>]
+// [<TestFixture>]
+[<AllureNUnit(false)>]
 [<AllureSuite("NUnit")>]
-[<AllureTag([| "NUnit"; |])>]
-// [<AllureSeverity(SeverityLevel.normal)>]
-[<AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")>]
-// [<AllureId(123)>]
-[<AllureName("test0001 NUnit")>]
+type testClass001() =
 
-[<Test>]
-[<TestCaseSource("factorialTestData")>]
-let factorialTest (number: int, expectedResult: int) =
-    let actualResult = FuncLib.factorial number
-    AreEqual(expectedResult, actualResult, $"Expected = {expectedResult}, actual = {actualResult}")
+    [<AllureEpic("Unit testing in F#")>]
+    [<AllureFeature([| "Parameterized tests" |])>]
+    [<AllureStory([| "NUnit" |])>]
+
+    [<AllureTag([| "NUnit" |])>]
+    // [<AllureSeverity(SeverityLevel.normal)>]
+    [<AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")>]
+    // [<AllureId(123)>]
+    [<AllureName("test0001 NUnit")>]
+
+
+    // [<Test>]
+    // [<TestCaseSource(typeof< testClass001>, "factorialTestData" )>]
+    // [<TestCaseSource("factorialTestData")>]
+    // [<TestCaseSource(typeof< testData>, "factorialTestData" )>]
+    [<TestCaseSource(typeof<testData>, nameof testData.factorialTestData)>]
+    // [<TestCaseSource( nameof factorialTestData )>]
+    // [<TestCaseSource(typeof SimpleLibNUnitTests, nameof factorialTestData )>]
+    // [<Description("NUnit parameterized test")>]
+    member this. factorialTest (number: int, expectedResult: int) =
+        let actualResult = FuncLib.factorial number
+        AreEqual(expectedResult, actualResult, $"Expected = {expectedResult}, actual = {actualResult}")
