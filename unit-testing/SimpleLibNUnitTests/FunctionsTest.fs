11fb6922344ffbdfc9bb13f7e27ab719278d0d8b
Aleksandr Petrovskiy <aepetrovskiy@inno.tech>
2022-11-13 21:09:50 +0300

    NUnit fixes

--- unit-testing/SimpleLibNUnitTests/FunctionsTest.fs
+++ unit-testing/SimpleLibNUnitTests/FunctionsTest.fs
@@ -1,77 +1,66 @@
-open NUnit.Framework
-
-type testData =
-
-    static member factorialTestData() = //: IEnumerable<TestCaseData> =
-        [ new TestCaseData(0, 1)
-          new TestCaseData(1, 1)
-          new TestCaseData(2, 2)
-          new TestCaseData(3, 6)
-          new TestCaseData(4, 24) ]
-
-[<TestFixture>]
+[<NUnit.Framework.TestFixture>]
 [<NUnit.Allure.Core.AllureNUnit>]
-[<NUnit.Allure.Attributes.AllureSuite("NUnit")>]
 
 [<NUnit.Allure.Attributes.AllureEpic("Unit testing in F#")>]
 [<NUnit.Allure.Attributes.AllureFeature([| "NUnit tests" |])>]
 [<NUnit.Allure.Attributes.AllureStory([| "NUnit" |])>]
 [<NUnit.Allure.Attributes.AllureSuite("NUnit")>]
 [<NUnit.Allure.Attributes.AllureTag([| "NUnit" |])>]
-module SimpleLibNUnitTests =
+
+// these two do not work)
+// module NUnit.Samples.AssertSyntaxTests2
+// module SimpleLibNUnitTests2
+module NUnit.Samples.SimpleLibNUnitTests
+
+open System
+open System.Collections
+open NUnit.Allure.Attributes
+open NUnit.Allure.Core
+open NUnit.Framework
+open NUnit.Framework.Constraints
+
+open type NUnit.Framework.Assert
+open SimpleLib
 
 
-    open System
-    open System.Collections.Generic
-    open Allure.Net.Commons
-    // open Allure.Commons
-    open NUnit.Allure.Attributes
-    open NUnit.Allure.Core
-    open NUnit.Framework
-
-    open type NUnit.Framework.Assert
+let factorialTestData () = //: IEnumerable<TestCaseData> =
+    [ new TestCaseData(0, 1)
+      new TestCaseData(1, 1)
+      new TestCaseData(2, 2)
+      new TestCaseData(3, 6)
+      new TestCaseData(4, 24) ]
 
 
-    open SimpleLib
-
-    // let factorialTestData () = //: IEnumerable<TestCaseData> =
-    //     [ new TestCaseData(0, 1)
-    //       new TestCaseData(1, 1)
-    //       new TestCaseData(2, 2)
-    //       new TestCaseData(3, 6)
-    //       new TestCaseData(4, 24) ]
 
 
-    [<Test>]
-    [<AllureName("simple NUnit test")>]
-    let someTest () =
-        let n = 3
-        AreEqual(n * n, FuncLib.square n)
+[<Test>]
+[<AllureName("simple NUnit test")>]
+let someTest () =
+    let n = 3
+    AreEqual(n * n, FuncLib.square n)
 
-    // [<TestFixture>]
-    // [<AllureNUnit>]
-    // [<AllureSuite("NUnit")>]
-    // type testClass001() =
+[<Test>]
+let oneMoreTest () = Assert.AreEqual(1, 1)
 
-    [<AllureEpic("Unit testing in F#")>]
-    [<AllureFeature([| "Parameterized tests" |])>]
-    [<AllureStory([| "NUnit" |])>]
+[<AllureEpic("Unit testing in F#")>]
+[<AllureFeature([| "Parameterized tests" |])>]
+[<AllureStory([| "NUnit" |])>]
 
-    [<AllureTag([| "NUnit" |])>]
-    // [<AllureSeverity(SeverityLevel.normal)>]
-    [<AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")>]
-    // [<AllureId(123)>]
-    [<AllureName("test0001 NUnit")>]
+[<AllureTag([| "NUnit" |])>]
+// [<AllureSeverity(SeverityLevel.normal)>]
+[<AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")>]
+// [<AllureId(123)>]
+[<AllureName("test0001 NUnit")>]
 
 
-    // [<Test>]
-    // [<TestCaseSource(typeof< testClass001>, "factorialTestData" )>]
-    // [<TestCaseSource("factorialTestData")>]
-    // [<TestCaseSource(typeof< testData>, "factorialTestData" )>]
-    [<TestCaseSource(typeof<testData>, nameof testData.factorialTestData)>]
-    // [<TestCaseSource( nameof factorialTestData )>]
-    // [<TestCaseSource(typeof SimpleLibNUnitTests, nameof factorialTestData )>]
-    // [<Description("NUnit parameterized test")>]
-    let factorialTest (number: int, expectedResult: int) =
-        let actualResult = FuncLib.factorial number
-        AreEqual(expectedResult, actualResult, $"Expected = {expectedResult}, actual = {actualResult}")
+// [<Test>]
+// [<TestCaseSource(typeof< testClass001>, "factorialTestData" )>]
+// [<TestCaseSource("factorialTestData")>]
+// [<TestCaseSource(typeof< testData>, "factorialTestData" )>]
+// [<TestCaseSource(typeof<testData>, nameof testData.factorialTestData)>]
+[<TestCaseSource(nameof factorialTestData)>]
+// [<TestCaseSource(typeof SimpleLibNUnitTests, nameof factorialTestData )>]
+[<Description("NUnit parameterized test")>]
+let factorialTest (number: int, expectedResult: int) =
+    let actualResult = FuncLib.factorial number
+    AreEqual(expectedResult, actualResult, $"Expected = {expectedResult}, actual = {actualResult}")
--- unit-testing/SimpleLibNUnitTests/UnitTest1.fs
+++ unit-testing/SimpleLibNUnitTests/UnitTest1.fs
@@ -1,16 +1,66 @@
 [<NUnit.Framework.TestFixture>]
 [<NUnit.Allure.Core.AllureNUnit>]
-module SampleTests
 
+[<NUnit.Allure.Attributes.AllureEpic("Unit testing in F#")>]
+[<NUnit.Allure.Attributes.AllureFeature([| "Syntax tests" |])>]
+[<NUnit.Allure.Attributes.AllureStory([| "Syntax" |])>]
+[<NUnit.Allure.Attributes.AllureSuite("Syntax")>]
+[<NUnit.Allure.Attributes.AllureTag([| "Syntax" |])>]
+module NUnit.Samples.AssertSyntaxTests1
+
+open System
+open System.Collections
 open NUnit.Allure.Attributes
 open NUnit.Allure.Core
 open NUnit.Framework
+open NUnit.Framework.Constraints
+
+/// <summary>
+/// This test fixture attempts to exercise all the syntactic
+/// variations of Assert without getting into failures, errors 
+/// or corner cases. Thus, some of the tests may be duplicated 
+/// in other fixtures.
+/// 
+/// Each test performs the same operations using the classic
+/// syntax (if available) and the constraint syntax. The
+/// inherited syntax is not used in this example, since it
+/// would require using a class to hold the tests, which
+/// seems to make it less useful in F#.
+/// </summary>
+
+[<Test>]
+[<AllureName("Assert.IsNull")>]
+let IsNull() =
+    let nada : obj = null
+    Assert.IsNull(nada)
+    Assert.That(nada, Is.Null)
+
+[<Test>]
+[<AllureName("Assert.IsNotNull")>]
+let IsNotNull() =
+    Assert.IsNotNull(42)
+    Assert.That(42, Is.Not.Null)
+
+// [<NUnit.Framework.TestFixture>]
+// [<NUnit.Allure.Core.AllureNUnit>]
+//
+// [<NUnit.Allure.Attributes.AllureEpic("Unit testing in F#")>]
+// [<NUnit.Allure.Attributes.AllureFeature([| "NUnit tests" |])>]
+// [<NUnit.Allure.Attributes.AllureStory([| "NUnit" |])>]
+// [<NUnit.Allure.Attributes.AllureSuite("NUnit")>]
+// [<NUnit.Allure.Attributes.AllureTag([| "NUnit" |])>]
+// module SampleTests
+//
+// open System
+// open System.Collections
+// open NUnit.Allure.Attributes
+// open NUnit.Allure.Core
+// open NUnit.Framework
+// open NUnit.Framework.Constraints
 
 [<SetUp>]
-let Setup () =
-    ()
+let Setup () = ()
 
 [<Test>]
 [<AllureName("basic NUnit test")>]
-let Test1 () =
-    Assert.Pass()
+let Test1 () = Assert.Pass()
